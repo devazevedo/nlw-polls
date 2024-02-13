c554a5408270340e313bd39eed95e3456bc511bc
azevedoafc <azevedoafc23@gmail.com>
2024-02-13 11:34:01 -0300

    commit final apis votacao com ws

---
+++ prisma/migrations/20240210213120_create_table_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "craetedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,27 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-result'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello'
+app.register(cookie, {
+  secret: "polls-app",
+  hook: "onRequest",
+  parseOptions: {}
 })
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
